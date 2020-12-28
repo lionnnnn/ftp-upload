6873cef653189c0b85a95ea6c93c034a5e9d2fea
lion <601035219@qq.com>
2020-12-28 22:22:06 +0800

    update: 完善单测 优化项目结构

---
+++ src/util/file_opr/index.js
@@ -0,0 +1,68 @@
+/*
+ * @Description: Description
+ * @Author: lion
+ * @Date: 2020-12-25 19:43:35
+ * @LastEditors: lion
+ * @LastEditTime: 2020-12-26 09:42:05
+ */
+
+const fs = require('fs');
+const glob = require('glob');
+const logger = require('../log/index.js');
+
+// 判断path是否是 目录
+function isDir(path) {
+    let stat = fs.statSync(path);
+    logger.debug('isDir', 'isDirectory', stat.isDirectory());
+    return stat.isDirectory();
+};
+
+// 根据路径(目录或者是文件名) 拿到所有的 file
+async function getFilePathFromPath(path) {
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            logger.info('start getfileFromPath fn..');
+            
+            let fileList = [];
+        
+            // glob解析 TODO: 目录嵌套目录需要递归
+            let filePaths = glob.sync(path, {});
+            
+            logger.debug('getFilePathFromPath', 'filePaths', filePaths);
+        
+            if (!filePaths.length) {
+                logger.error('getFilePathFromPath', 'can not find any files from the path!');
+                resolve([]);
+            }
+        
+            for (let filePath of filePaths) {
+                let flag = isDir(filePath);
+        
+                // 如果 是 文件目录 需要取出文件
+                if (flag) {
+                    let files = fs.readdirSync(path);
+        
+                    files.forEach(file => {
+                        fileList.push(file);
+                    });
+                } else {
+                    fileList.push(filePath);
+                }
+            }
+        
+            logger.debug('getFilePathFromPath', 'fileList', fileList);
+            resolve(fileList);
+        } catch (err) {
+            logger.error('getFilePathFromPath', err);
+            reject();
+        }
+    })
+};
+
+
+
+module.exports = {
+    isDir,
+    getFilePathFromPath
+};
--- test/ftp.test.js
+++ test/ftp.test.js
@@ -1,17 +1,57 @@
 /*
  * @Description: Description
- * @Author: jacksonyyy
+ * @Author: lion
  * @Date: 2020-12-26 10:17:06
- * @LastEditors: jacksonyyy
- * @LastEditTime: 2020-12-26 14:01:36
+ * @LastEditors: lion
+ * @LastEditTime: 2020-12-26 17:49:53
  */
 
-const ftp = require('../ftp/index.js');
-let client = new ftp.ftp();
+const ftp = require('ftp');
+
+const uploader = require('../src/uploader/index.js');
+
+
+jest.mock('ftp', () => {
+    return jest.fn().mockImplementation(() => {
+
+        return {
+            connect: jest.fn(() => {}),
+            put: jest.fn((path, destPath, cb) => {
+                if (path === './upload_files/' && destPath === '/yjc/') {
+                    cb();
+                } else {
+                    cb('no such destination path');
+                }
+
+            }),
+            list: jest.fn((destPath, cb) => {
+                if (destPath[0] === '/yjc/') {
+                    cb(null, [{
+                        "date": '2020-12-12T07:37:00.000Z',
+                        "name": "text1.txt",
+                        "size": 9,
+                        "type": "-",
+                    }]);
+                } else {
+                    cb({
+                        code: 550
+                    })
+                }
+            }),
+            end: jest.fn(() => {}),
+            on: jest.fn((status, cb) => {
+                if (status && cb) {
+                    cb();
+                }
+            })
+        };
+    });
+}); 
+
 
 test('login 用户名密码正确', async() => {
     expect.assertions(1);
-    const data = await ftp.login(client, {
+    const data = await uploader.connect({
         host: '47.107.157.97',
         port: 21,
         user: 'ftp',
@@ -25,30 +65,89 @@ test('login 用户名密码正确', async() => {
     });
 });
 
-test('login 数据为空', async() => {
+test('login 部分数据为空', async() => {
     expect.assertions(1);
-    const data = await ftp.login(client, {
+
+    await uploader.connect({
         host: '',
         port: 21,
         user: '',
         password: ''
     }).catch((err) => {
-        expect(err).toBe('login failed, please correct and retry');
-
+        expect(err).toBe();
     });
+
 });
 
-// test('测试jest.fn()返回Promise', async() => {
-//     let mockFn = jest.fn(ftp.login).mockResolvedValue('default');
-//     let result = await mockFn();
-//     // 断言mockFn通过await关键字执行后返回值为default
-//     expect(result).toBe('default');
-//     // 断言mockFn调用后返回的是Promise对象
-//     expect(Object.prototype.toString.call(mockFn())).toBe("[object Promise]");
-// })
+test('list 远程路径正确', async() => {
+    expect.assertions(1);
+    const data = await uploader.list('/yjc/');
+    expect(data).toEqual([{
+        "date": '2020-12-12T07:37:00.000Z',
+        "name": "text1.txt",
+        "size": 9,
+        "type": "-",
+    }]);
+});
 
-// test('list 远程地址存在', async() => {
-//     expect.assertions(1);
-//     const data = await ftp.list(client, '/ycy/');
-//     expect(data).toBe(['yjc']);
-// });
+test('list 远程路径为空', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.list('').catch((err) => {
+        expect(err).toBe('[list error]: destination path can not be empty');
+    });
+
+});
+
+test('list 远程路径不存在', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.list('xxx').catch((err) => {
+        expect(err).toBe('[list error]: The system cannot find the files specified.');
+    });
+
+});
+
+test('upload 本地路径为空', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.upload('', '/yjc/').catch((err) => {
+        expect(err).toBe('[upload] path can not be empty!');
+    });
+
+});
+
+test('upload 本地路径不存在', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.upload('xxx', '/yjc/').catch((err) => {
+        expect(err).toEqual('[upload error]: can not find files to upload');
+    });
+
+});
+
+test('upload 远程路径为空', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.upload('./upload_files/', '').catch((err) => {
+        expect(err).toEqual('[upload] destination path can not be empty!');
+    });
+
+});
+
+
+test('upload 上传成功', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.upload('./upload_files/', '/yjc/');
+    expect(data).toEqual([ 'szy20201219.txt', 'test20201212-1.txt', 'test502-1.txt' ]);
+
+});
+
+test('close 关闭服务成功', async() => {
+    expect.assertions(1);
+
+    const data = await uploader.close();
+    expect(data).toEqual();
+
+});
